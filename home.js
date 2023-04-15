9fe46a6f8465338e65fee7eeb29c1434c072e035
CODRAU <cipirau@gmail.com>
2023-04-15 13:02:51 -0400

    back to top button

--- home.js
+++ home.js
@@ -2,3 +2,8 @@ function downpress() {
     document.getElementById("aboutus").scrollIntoView(true);
     return 0;
 }
+
+function uppress() {
+    document.getElementsByClassName("navbar")[0].scrollIntoView(true);
+    return 0;
+}
