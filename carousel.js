0e45ce98fddf213f4a070ebbe98615b21459cd7b
CODRAU <cipirau@gmail.com>
2023-04-25 19:33:22 -0400

    carousel feature

---
+++ carousel.js
@@ -0,0 +1,34 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+var cards = ["content", "first", "second", "third"];
+var currIndex = 0;
+
+//leftpress
+function downpress() {
+    if (currIndex != 0) {
+        document.getElementById(cards[currIndex]).classList.remove('show');
+        currIndex -= 1;
+        document.getElementById(cards[currIndex]).classList.add('show');
+    }
+    return 0;
+}
+
+//rightpress
+function uppress() {
+    if (currIndex != 3) {
+        document.getElementById(cards[currIndex]).classList.remove('show');
+        currIndex += 1;
+        document.getElementById(cards[currIndex]).classList.add('show');
+    }
+    return 0;
+}
