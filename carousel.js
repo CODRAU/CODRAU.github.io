33732087f1b2043e0b8f672f46701b00375e9323
CODRAU <cipirau@gmail.com>
2023-04-25 20:11:21 -0400

    eugbwhp9uqehwrgfipuqerlgvb

--- carousel.js
+++ carousel.js
@@ -1,15 +1,3 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-
 var cards = ["c", "first", "second", "third"];
 var currIndex = 0;
 
